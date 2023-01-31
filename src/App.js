a568ade7403eabb3eb8d62bb69c0ca172d362650
beatrixiez <bsmbarreto@gmail.com>
2023-01-31 17:06:19 -0300

    cards updated

--- src/App.js
+++ src/App.js
@@ -91,8 +91,8 @@ function App() {
 <div className='card__info'>
   <h4>{home.attributes[0].value}</h4>
   <p>
-    <strong>{home.attributes[2].value}</strong> bds |
-    <strong>{home.attributes[3].value}</strong> ba |
+    <strong>{home.attributes[2].value}</strong> bedrooms |
+    <strong>{home.attributes[3].value}</strong> bathrooms |
     <strong>{home.attributes[4].value}</strong> sqft
   </p>
   <p>{home.address}</p>
